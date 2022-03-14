25ee40adba360fc5f794d296ad4ff1a7c5827ac9
Alexys <66492231+AlexysGromard@users.noreply.github.com>
2022-03-14 12:50:07 +0100

    init screen

--- main/screen.cpp
+++ main/screen.cpp
@@ -1,5 +1,13 @@
 #include "screen.h"
+SoftwareSerial nextionSerial(10, 11); // RX, TX
 
 int CommeCa(int paremetre) { // Meme signature (nom / parametres) mais on déclare un corps
   return 1546;
+}
+
+void init(){
+  Serial.begin(9600);
+  Serial.print("Serial port has been set to 9600")
+  nextionSerial.begin(9600);
+  Serial.print("nextionSerial port has been set to 9600")
 }
--- main/screen.h
+++ main/screen.h
@@ -2,7 +2,10 @@
 #define SCREEN_H
 
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
 int CommeCa(int parametre); // Pas de définition, juste le nom / parametres
+
+
 
 #endif
