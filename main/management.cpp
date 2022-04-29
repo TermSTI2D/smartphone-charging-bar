aba7686b56644e304d41d355e6763fd37712263d
PopTurtle <91342591+PopTurtle@users.noreply.github.com>
2022-04-29 12:40:53 +0200

    Update management.cpp

--- main/management.cpp
+++ main/management.cpp
@@ -9,33 +9,20 @@
 byte _aimedPos = 0;
 
 Phone phones[8] = {
-  {0, true, "0000"},
-  {1, true, "0000"},
-  {2, true, "0000"},
-  {3, true, "0000"},
-  {4, true, "0000"},
-  {5, true, "0000"},
-  {6, true, "0000"},
-  {7, true, "0000"},
+  { true, "0000" },
+  { true, "1324" },
+  { true, "5678" },
+  { true, "9070" },
+  { true, "0000" },
+  { true, "0000" },
+  { true, "0000" },
+  { true, "0000" },
 };
 
+
+
 void InitManagement() {
-  
-  
-  // Load data
-  //Serial.println( sizeof(Phone) );
-  //Serial.println(phones[0].isEmpty);
-
-  LoadPhones();
-  //SavePhones();
-  for (int i = 0; i < 8; i++) {
-    Phone p = phones[i];
-
-    Serial.print(String(i) + ": " + String(p.id) + " " + String(p.isEmpty) + " " + p.password);
-    Serial.println("");
-  }
-
-  Serial.println("Loaded");
+  LoadPhones(PHONES_MEMORY, phones, sizeof(phones));
 }
 
 void SetAimedPos(byte newPos) {
@@ -46,40 +33,6 @@ byte GetAimedPos() {
   return _aimedPos;
 }
 
-void LoadPhones() {
-  char phonestxt[PHONELINE*8];
-  LoadData("phones.txt", "NoData").toCharArray(phonestxt, PHONELINE*8);
-
-  char * line = strtok(phonestxt, "\n");
-  
-  for (int i = 0; i < 8; i++) {
-    // Unpack the data
-    char * data = strtok(line, ",");
-    phones[i].id = atoi(data);
-    
-    data = strtok(0, ",");
-    phones[i].isEmpty = (bool)atoi(data);
-    
-    data = strtok(0, ",");
-    strcpy(phones[i].password, data);
-    
-    // Setup next data
-    line = strtok(line + PHONELINE, "\n");
-    if (line == 0) break;
-  }
-}
-
-void SavePhones() {
-  String phonestxt = "";
-  for (int i = 0; i < 8; i++) {
-    Phone *p = &phones[i];
-    phonestxt += String(p->id) + "," + String(p->isEmpty) + "," + p->password + "\n";// + "," + p->isEmpty + "," + p->password + "\n";
-  }
-
-  //Serial.println(phonestxt);
-  SaveData("phones.txt", phonestxt);
-}
-
 bool RegisterPhone(byte id, String password) {
   Phone *p = &phones[id];
 
@@ -87,7 +40,7 @@ bool RegisterPhone(byte id, String password) {
     p->isEmpty = false;
     password.toCharArray(p->password, 5);
     
-    SavePhones();
+    SavePhones(PHONES_MEMORY, phones, sizeof(phones));
     return true;
   }
   return false;
@@ -99,7 +52,7 @@ bool RecoverPhone(byte id) {
   if (!p->isEmpty) {
     p->isEmpty = true;
     strcpy(p->password, "0000");
-    SavePhones();
+    SavePhones(PHONES_MEMORY, phones, sizeof(phones));
     return true;
   }
   return false;
